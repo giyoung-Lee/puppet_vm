905818b4d269ae325458694ffc2ca2d035292528
Lee giyoung <rldud112@naver.com>
2016-05-27 16:02:17 +0900

    Added file resource to testserver

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,4 +1,15 @@
-node 'soya-W94-95-97SU2-SUY-C-T' {
-	include sys_info
+node '192.168.1.2' {
+#	include sys_info
+}
+
+node '192.168.122.133' {
+	file {'/tmp/testserver':
+		content => "This is a test server. \n",
+	}
+}
+
+node /^vm[0-9]+\.linuxmaster\.com$/ {
+#include vim_upggrade
+#include sshd
 }
 
